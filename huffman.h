8a40e3f259799fbb1dbd5430812220dae16932c3
Maxwell Wang <maxwellwang01@gmail.com>
2020-03-26 17:38:42 -0400

    did franny library stuff

--- huffman.h
+++ huffman.h
@@ -1,5 +1,5 @@
-#ifndef _HUFFMAN_DATA_STRUCTURES_H
-#define _HUFFMAN_DATA_STRUCTURES_H
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
 
 // Can represent either a word with frequency or a tree of words
 typedef struct _node {
@@ -16,27 +16,27 @@ typedef struct _heap {
 } Heap;
 
 // Makes token node
-Node* makeTokenNode(char* token, int frequency);
+Node* makeTokenNode(char*, int);
 
 // Sifts up final node in heap to correct position
-void siftUp(Heap* aHeap);
+void siftUp(Heap*);
 
 // Inserts node into heap
-void insertNode(Heap* aHeap, Node* aNode);
+void insertNode(Heap*, Node*);
 
 // Sifts down first node in heap to correct position
-void siftDown(Heap* aHeap);
+void siftDown(Heap*);
 
 // Extracts minimum node from heap
-Node* extractNode(Heap* aHeap);
+Node* extractNode(Heap*);
 
 // Builds tree from 2 nodes
-Node* buildTree(Node* nodeA, Node* nodeB);
+Node* buildTree(Node*, Node*);
 
 // Builds Huffman tree from min heap
-Node* buildHuffmanTree(Heap* aHeap);
+Node* buildHuffmanTree(Heap*);
 
 // Builds codebook from Huffman tree
-void buildCodebook(Node* aNode);
+void buildCodebook(Node*);
 
 #endif
